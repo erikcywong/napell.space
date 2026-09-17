d9378fe1d0e76040edb0148bdfb287546829c92b
erikcywong <erik.cywong@outlook.com>
2026-09-18 05:33:42 +0800

    Add Vision page: full coffee chain intro with dashboard, empowerment flywheel, benchmarks and roadmap (trilingual)

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ function renderNav(activePage) {
     { id: 'efficiency', key: 'nav_efficiency', href: 'efficiency.html' },
     { id: 'value-chain', key: 'nav_value_chain', href: 'value-chain.html' },
     { id: 'collaboration', key: 'nav_collaboration', href: 'collaboration.html' },
+    { id: 'vision', key: 'nav_vision', href: 'vision.html' },
     { id: 'contact', key: 'nav_contact', href: 'contact.html' }
   ];
 
